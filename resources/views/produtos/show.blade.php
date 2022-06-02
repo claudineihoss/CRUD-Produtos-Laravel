3603551b44c997149bacc55a347bb56190fc2b73
“Claudinei <claudineihoss@yahoo.com.br>
2022-06-02 08:46:13 -0300

    “primeirocommit”-

---
+++ resources/views/produtos/show.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.template')
+@section('title', 'Produto')
+@section('content')
+<?php
+$valor_prod = number_format($produto->valor, 2, ',', '.');
+?>
+<div class="jumbotron">
+  <h1 class="display-4"><?php echo $produto->nome; ?> </h1>
+  <p class="lead"><?php echo $produto->descricao; ?> - Valor R$ <?php echo $valor_prod; ?></p>
+  <hr class="my-4">
+  <p><?php echo $produto->descricao_longa; ?></p>
+  <a class="btn btn-primary btn-lg" href="{{route('produtos')}}" role="button">Ver Produtos</a>
+</div>
+@endsection
